65a00bdbed4c73fb6690598394d96137077a3635
Carlos Delgadillo <carlos.degadillo102003@gmail.com>
2024-07-01 12:16:00 -0600

    s

--- test_suma.py
+++ test_suma.py
@@ -8,5 +8,5 @@ client = TestClient(app)
 def test_sumar():
     response = client.post("/sumar", json={"num1": 10, "num2": 5})
     assert response.status_code == 200
-    assert response.json() == 15
+    assert response.json() == 10
 
